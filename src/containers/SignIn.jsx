86c0ee1c3cfd15e1d373dfbe69fb84969fc35742
mateus ravedutti <mateusravedutti@gmail.com>
2017-02-12 02:32:14 -0200

    slightly modification on signin component

--- src/containers/SignIn.jsx
+++ src/containers/SignIn.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { EmailSignInForm } from "redux-auth/default-theme";
 import { browserHistory } from 'react-router';
-export class SignIn extends React.Component {
+class SignIn extends React.Component {
   render() {
     return <EmailSignInForm next={() => console.log("success")} />
   }
 }
-//export default connect(({ routes }) => ({ routes }))(SignIn);
+export default connect(({ routes }) => ({ routes }))(SignIn);
