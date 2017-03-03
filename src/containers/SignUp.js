d53d3924f2788702f7f7e20262f3bae4d359845e
Pedro Henrique! <pha14@inf.ufpr.br>
2017-03-03 11:13:20 -0300

    Implements layout for signup cep
    Signed-off-by: Pedro Henrique! <pha14@inf.ufpr.br>

---
+++ src/containers/SignUp.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, Card, Row, Col } from 'react-materialize';
+import styles from './styles/SignUp.css';
+import MaskedInput from 'react-maskedinput';
+
+export const SignUpCEP = () => (
+		<div className='card'>
+			<form>
+				<div className='card-content'>
+					<h2 className='card-title'>Digite seu CEP</h2>
+					<p>Para podermos verificar a disponibilidade do agendador na sua cidade, informe seu CEP.</p>
+					<MaskedInput 
+						placeholder='CEP'
+						mask='11111-111' />
+				</div>
+				<div className='card-action'>
+					<button className='btn waves-effect right'> Enviar </button>
+					<a className='back-bt waves-effect btn-flat'> Voltar </a>
+				</div>
+			</form>
+		</div>
+)
+
+
+class SignUp extends React.Component {
+	render () {
+		return(
+			<main className={styles['main']}>
+				<div className='container'>
+					<Row className={styles['cep']}>
+              			<Col s={12} m={12} l={12} >
+              				<SignUpCEP />
+              			</Col>
+              		</Row>
+              	</div>			
+			</main>
+		)
+	}
+}
+
+export default SignUp
