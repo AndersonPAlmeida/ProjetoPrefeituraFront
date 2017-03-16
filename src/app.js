fd55ac3673242026bb44f8e5a36a968421c7ab65
Mateus Ravedutti <mrlm15@inf.ufpr.br>
2017-03-16 11:58:10 -0300

    Fix state for server rendering

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import createHistory from 'react-router/lib/createMemoryHistory';
-import { createStore } from './redux/createStore';
 import { IndexRoute, Route, Router, browserHistory } from 'react-router';
 import { App, Home, NotFound } from './containers';
 import { PageOne, PageTwo } from './containers';
 import { configure } from './redux-auth';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
 function requireAuth(store, nextState, replace, next) {
   if (!store.getState().auth.getIn(['user', 'isSignedIn'])) {
     replace('/');
@@ -14,10 +16,25 @@ function requireAuth(store, nextState, replace, next) {
   next();
 }
 export function initialize({ apiUrl, cookies, isServer, currentLocation, userAgent } = {}) {
+  const reducer = require('./reducers');
   /* Start history with requested url */
   let memoryHistory = isServer ? createHistory(currentLocation) : browserHistory;
+  const middleware = [
+    routerMiddleware(memoryHistory),
+    thunk,
+  ];
   /* Create store and enhanced history (memoryHistory) */
-  const store = createStore(memoryHistory);
+  const store = createStore(
+    reducer,
+    compose(
+      applyMiddleware(...middleware)
+    )
+  );
+  if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('./reducers', () => {
+      store.replaceReducer(require('./reducers'));
+    });
+  }
   let history = syncHistoryWithStore(memoryHistory, store);
   const routes = (
     <Router history={history}>
