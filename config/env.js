0dd453b34f2472295e6291c220a5c5dc449f8604
Pedro Henrique! <pha14@inf.ufpr.br>
2017-04-13 09:50:41 -0300

    fix ports on env congif
    Signed-off-by: Pedro Henrique! <pha14@inf.ufpr.br>

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 module.exports = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3000,
   apiHost: 'localhost',
-  apiPort: 3000,
+  apiPort: 5000,
   apiVer: 'v1',
   webpackHost: 'localhost',
   webpackPort: 3001,
