25a4d94e2ff41c5426edd7dd6af4cf666724c226
Kardi <pha14@inf.ufpr.br>
2017-04-25 12:07:55 -0300

    Implements navbar
    Signed-off-by: Kardi <pha14@inf.ufpr.br>

--- src/containers/Common.js
+++ src/containers/Common.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
 import LogoImage from '../../public/logo.png'
 import styles from './styles/Home.css'
-import { Button, Card, Row, Col } from 'react-materialize'
+import { Button, Card, Row, Col, Dropdown, NavItem } from 'react-materialize'
 import McImg from '../../public/mc_logo.png'
 import LogoImg from '../../public/logo.png'
+import UserImg from '../../public/user.png'
 
 
 export const GovernmentBar = () => (
@@ -46,13 +47,56 @@ export const Footer = () => (
 
 export const NavBar = () => (
     <nav className="white">
-      <div className="nav-wrapper container">
-        <div className="brand-logo center-align">   
-          <img
-               className={styles['nav-logo']}
-               src={LogoImg} />
-          <h2 className='right city-name'> São José dos Pinhais </h2>
+      <div className={styles['nav-width'] + " nav-wrapper container"}>
+        <div className="nav-brand center-align">   
+          <a>
+            <img
+                 className={styles['nav-logo']}
+                 src={LogoImg} />
+          </a>
+            <h2 className="city-name right hide-on-med-and-down left-align"> São José dos Pinhais </h2>
         </div>
+        <a className="right black-text logout-icon modal-trigger" title="Sair" data-target="">
+          <i className="material-icons">exit_to_app</i>
+        </a>
+        <a data-activates="notifications-menu" href="" className="button-notifications black-text right" title="Notificações">
+          <span className="mdl-badge no-notification" data-badge="0">
+            <i className="material-icons" id="notification-icon">notifications</i>
+          </span>
+        </a>
+        <ul className="right hide-on-med-and-down large-menu" id="nav-ul">
+          <li>
+            <a href="">Efetuar Agendamento</a>
+          </li>
+          <li>
+            <a href="">Histórico</a>
+          </li>
+          <li>
+            <Dropdown trigger={
+                <a>
+                  <span className="user-name left">Administrador MPOG</span>
+                  <img 
+                    alt="Administrador MPOG" 
+                    className="material-icons circle profile-pic right" 
+                    src={UserImg} />
+                  <i className="material-icons right">arrow_drop_down</i> 
+                </a>
+              }>
+              <NavItem>Cidadão<p className='nav-p'>Brasília</p></NavItem>
+              <NavItem>Mudar Permissão</NavItem>
+              <NavItem divider />
+              <NavItem>Editar</NavItem>
+              <NavItem>Dependentes</NavItem>
+              <NavItem>Imprimir Cadastro</NavItem>
+              <NavItem divider />
+              <NavItem>Sair</NavItem>
+            </Dropdown>
+
+            
+          </li>
+        </ul>
+      </div>
+      <div>
       </div>
       <div className="progress">
         <div></div>
