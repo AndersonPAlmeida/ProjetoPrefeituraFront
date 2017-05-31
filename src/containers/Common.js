30ba549886c2fea7d0516458b15c39791ec7666e
Mateus Ravedutti <mrlm15@inf.ufpr.br>
2017-05-31 11:30:10 -0300

    Update footer component

--- src/containers/Common.js
+++ src/containers/Common.js
@@ -3,6 +3,7 @@ import LogoImage from '../../public/logo.png'
 import styles from './styles/Home.css'
 import { Button, Card, Row, Col } from 'react-materialize'
 import McImg from '../../public/mc_logo.png'
+import { browserHistory } from 'react-router';
 
 export const GovernmentBar = () => (
         <div className="show-on-large-only government-bar"> 
@@ -22,22 +23,29 @@ export const Header = () => (
           </div>
         </div>
 )
-export const Footer = () => (
-        <div className={styles['footer']}>
-          <div className={styles['top-footer']}>
-            <Row>
-              <Col s={12} m={12} l={12}>
-                <div>
-                  <p> Manual </p>
-                </div>
-              </Col>
-            </Row>
+export const Footer = (props) => 
+{
+  const items = props.footerItems;
+  const listItems = items.map((item, idx) =>
+    <a key={idx} className='back-bt waves-effect btn-flat' onClick={() => browserHistory.push(item.link)} > {item.name} </a>
+  );
+  return  (
+          <div className={styles['footer']}>
+            <div className={styles['top-footer']}>
+              <Row>
+                <Col s={12} m={12} l={12}>
+                  <div>
+                    {listItems}
+                  </div>
+                </Col>
+              </Row>
+            </div>
+            <div>
+              <img
+                className={styles['mc-img']}
+                src={McImg}
+              />
+            </div>
           </div>
-          <div>
-            <img
-              className={styles['mc-img']}
-              src={McImg}
-            />
-          </div>
-        </div>
-)
+  )
+}
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,13 +2,19 @@ import React, {Component} from 'react'
 import {GovernmentBar, Header, Footer} from './Common.js'
 export default class Home extends React.Component {
   render() {
+  const footerItems = [
+                        { 'name': 'Perguntas e Respostas', 'link': '/agendador/faq' },
+                        { 'name': 'Contato', 'link': '/agendador/contact' },
+                        { 'name': 'Comunicar um erro', 'link': '/agendador/report' },
+                        { 'name': 'Manual', 'link': '/agendador/manual' }
+                      ]
     return (
       <div>
         <GovernmentBar />
         <div>
           <Header />
           {this.props.children}
-          <Footer />
+          <Footer footerItems={footerItems} />
         </div>
       </div>
     )
