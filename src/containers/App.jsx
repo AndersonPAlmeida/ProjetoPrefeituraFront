f725ffcc5f00267467f2b23d96478a4ef9de80b3
Mateus Ravedutti <mrlm15@inf.ufpr.br>
2017-06-13 11:01:30 -0300

    Add navigation history

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,6 +3,9 @@ import { AuthGlobals } from "../redux-auth/views/default";
 import PropTypes from 'prop-types';
 import Home from './Home';
 import { connect } from 'react-redux'
+import { browserHistory } from 'react-router';
+import { Link } from 'react-router';
+
 class getApp extends React.Component {
   render() {
     const footerItems = [
@@ -11,6 +14,9 @@ class getApp extends React.Component {
                           { 'name': 'Comunicar um erro', 'link': '/agendador/report' },
                           { 'name': 'Manual', 'link': '/agendador/manual' }
                         ]
+    const navHistory = this.props.path.map((item, idx) => {
+        return <div style={ { display: 'inline' } }> { item.link == '/' ? '' : ' > ' } <Link key={idx} to={item.link}>{item.name}</Link></div>
+    });
     return ( 
       <div> 
         <AuthGlobals />
@@ -18,6 +24,7 @@ class getApp extends React.Component {
           footerItems={ this.props.is_authenticated ? footerItems : [ footerItems[3] ] }
           showHeader={ this.props.is_authenticated ? false : true }
         >
+          { this.props.is_authenticated ? navHistory : <div /> }
           {this.props.children} 
         </Home>
       </div>
@@ -28,8 +35,16 @@ getApp.propTypes = {
   children: PropTypes.any,
 };
 const mapStateToProps = (state) => {
+  var path = [ { 'name': 'Home', 'link': '/' } ] 
+  var arr_path = state.routing.locationBeforeTransitions.pathname.split('/').filter(Boolean)
+  var str_path = ''
+  arr_path.map(function(item) {
+    str_path = str_path + '/' + item
+    path.push( { 'name': item, 'link': str_path } )
+  })
   return {
-    is_authenticated: state.auth.getIn(['user', 'isSignedIn']) 
+    is_authenticated: state.auth.getIn(['user', 'isSignedIn']),
+    path
   }
 }
 const App = connect(
