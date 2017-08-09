2cf0136e623f0588971e3966bb22bceb690eef75
Pedro Henrique! <pha14@inf.ufpr.br>
2017-08-09 10:49:16 -0300

    fix minors issues
    Signed-off-by: Pedro Henrique! <pha14@inf.ufpr.br>

--- src/containers/application/ScheduleAgreement.jsx
+++ src/containers/application/ScheduleAgreement.jsx
@@ -30,35 +30,35 @@ class ScheduleAgreement extends Component {
     });
   }
 
-	mainComponent() {
-		return (
-			<div className='card'>
-	          <div className='card-content'>
-	            <h2 className='card-title h2-title-home'> Passo 1 de 3 - Termo de Compromisso </h2>
-	            <ul className="collection">
-					{this.scheduleRules()}
-			 	</ul>
-	          </div>
-	          {this.agreeButton()}
-	    	</div>
-		)
-	}
+  mainComponent() {
+    return (
+      <div className='card'>
+            <div className='card-content'>
+              <h2 className='card-title h2-title-home'> Passo 1 de 3 - Termo de Compromisso </h2>
+              <ul className="collection">
+          {this.scheduleRules()}
+        </ul>
+            </div>
+            {this.agreeButton()}
+        </div>
+    )
+  }
 
-	agreeButton() {
-		return (
-			<div className="card-action">
-				<button className="btn waves-effect btn" name="anterior" type="submit">Não concordo</button>
-				<button onClick={() =>browserHistory.push('/schedule_choose')} className="waves-effect btn right" name="commit" type="submit">Concordar e continuar</button>
-	    	</div>
-		)
-	}
+  agreeButton() {
+    return (
+      <div className="card-action">
+        <button className="btn waves-effect btn" name="anterior" type="submit">Não concordo</button>
+        <button onClick={() =>browserHistory.push('/schedule_choose')} className="waves-effect btn right" name="commit" type="submit">Concordar e continuar</button>
+        </div>
+    )
+  }
 
-	sectorList() {
-		const sectorsAbsence = (
+  sectorList() {
+    const sectorsAbsence = (
       this.state.sectors.map((sector) => {
         return (
           <li>
-            <b>{sector.name}</b>: {sector.absence_max} {sector.absence_max > 1 ? 'faltas' : 'falta'}
+            - <b>{sector.name}</b>: {sector.absence_max} {sector.absence_max > 1 ? 'faltas' : 'falta'}
           </li>
         )
       })
@@ -67,41 +67,40 @@ class ScheduleAgreement extends Component {
       <ul className="sectors_list">
         {sectorsAbsence}
       </ul>
-		)
-    return sectorsAbsence
-	}
+    )
+  }
 
-	scheduleRules() {
-		return (
-		<li className="collection-item agree-itens">
-			<p>Caso efetue o agendamento e não compareça ao local do atendimento no dia e
-			horário agendado, uma falta será registrada para você no setor do agendamento
-			escolhido.</p>
-			<p>Cada setor possui um número máximo de faltas, ultrapassar esse limite dentro
-			<b> 90</b> dias irá bloquear que você
-			solicite agendamentos pela internet pelos próximos
-			<b> 30</b> dias.</p>
+  scheduleRules() {
+    return (
+    <li className="collection-item agree-itens">
+      <p>Caso efetue o agendamento e não compareça ao local do atendimento no dia e
+      horário agendado, uma falta será registrada para você no setor do agendamento
+      escolhido.</p>
+      <p>Cada setor possui um número máximo de faltas, ultrapassar esse limite dentro
+      <b> 90</b> dias irá bloquear que você
+      solicite agendamentos pela internet pelos próximos
+      <b> 30</b> dias.</p>
 
-			<p>Os setores e seus respectivos limites de faltas estão listados abaixo:</p>
-				 		
-			{this.sectorList()}
- 		</li>
+      <p>Os setores e seus respectivos limites de faltas estão listados abaixo:</p>
+            
+      {this.sectorList()}
+    </li>
 
-		)
-	}
+    )
+  }
 
   render() {
     return (
       <div>
-	      <main>
-	      	<Row>
-		        <Col s={12}>
-			      	<div>
-			      		{this.mainComponent(this.state.sectors)}
-			      	</div>
-		      	</Col>
-		    </Row>
-		  </main>
+        <main>
+          <Row>
+            <Col s={12}>
+              <div>
+                {this.mainComponent(this.state.sectors)}
+              </div>
+            </Col>
+        </Row>
+      </main>
       </div>
     )
   }
