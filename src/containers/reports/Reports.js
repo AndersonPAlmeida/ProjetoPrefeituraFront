5f981e2350ab66496e16586a711805d60520c137
André Starosta <afas15@inf.ufpr.br>
2017-12-08 10:13:03 -0200

    AGILE#261: Add report types menu

--- src/containers/reports/Reports.js
+++ src/containers/reports/Reports.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router'
 import { browserHistory } from 'react-router'
 import { port, apiHost, apiPort, apiVer } from '../../../config/env'
 import {parseResponse} from "../../redux-auth/utils/handle-fetch-response"
-import {Row, Col, Table} from 'react-materialize'
+
+import {Row, Col, Table, Input, Button} from 'react-materialize'
 import {fetch} from "../../redux-auth";
 import { connect } from 'react-redux'
 import { findDOMNode } from 'react-dom';
 import ReactDOMServer from 'react-dom/server';
 import ReactDOM from 'react-dom';
 import html2canvas from 'html2canvas';
-import styles from './styles/MyReport.css';
+import styles from './styles/Reports.css';
 import { LogoImage } from '../images'
 var jsPDF
 
@@ -18,20 +19,66 @@ var jsPDF
 class getReports extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+    reportType: [],
+    reportStatus: 0
+      }
+      this.handleDecisionChange = this.handleDecisionChange.bind(this)
+      this.confirmReportType = this.confirmReportType.bind(this)
 
-  }
-  componentDidMount(){
-    console.log(this.props.user)
-    jsPDF = require('jspdf')
+    }
 
-  }
-
-
-
-  render() {
+render() {
     return (
-      <div>Teste</div>
+      <div className="card selectReport">
+        <div className="card-content">
+          <h5>Selecione o Tipo de relatório</h5>
+          <Row>
+          <Input s={6} type='select' label="Tipo do relatório" defaultValue='0' onChange={this.handleDecisionChange}>
+                  {this.getReportTypeList().map(function(element){
+                      return(<option value={element.value}>{element.name}</option>);
+                  })}
+          </Input>
+          </Row>
+          <Button offset="s2" onClick={this.confirmReportType}>Continuar</Button>
+        </div>
+      </div>
     )
+  }
+
+  getReportTypeList(){
+    var list = [
+      {"value":0,"name":"Agendamentos"},
+      {"value":1,"name":"Atendimentos"},
+      {"value":2,"name":"Cidadãos"},
+      {"value":3,"name":"Escalas"},
+      {"value":4,"name":"Número de atendimentos por tipo"},
+      {"value":5,"name":"Profissionais"}
+    ]
+    return(list)
+  }
+
+  handleDecisionChange(e){
+    this.setState({"reportType":e.target.value})
+    console.log(this.state)
+  }
+
+  confirmReportType(){
+    switch(this.state.reportType){
+      case "0":
+
+      case "1":
+      
+      case "2":
+      
+      case "3":
+      
+      case "4":
+      
+      case "5":
+      
+    }
+
   }
 }
 
