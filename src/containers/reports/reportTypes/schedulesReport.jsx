6c3c4bb69f8ae50c7d4a46c09ee21e9dc839796d
André Starosta <afas15@inf.ufpr.br>
2017-12-12 11:50:35 -0200

    AGILE#261: Add table fields

--- src/containers/reports/reportTypes/schedulesReport.jsx
+++ src/containers/reports/reportTypes/schedulesReport.jsx
@@ -17,25 +17,29 @@ class getSchedulesReport extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      citizensList: []
+      schedulesList: []
     }
-    this.getCitizensList = this.getCitizensList.bind(this)
-    this.returnCitizenList = this.returnCitizenList.bind(this)
+    this.getSchedulesList = this.getSchedulesList.bind(this)
+    this.returnSchedulesList = this.returnSchedulesList.bind(this)
+    this.formatDateTime = this.formatDateTime.bind(this)
   }
 
   componentDidMount(){
     jsPDF = require('jspdf')
+    this.returnSchedulesList()
   }
-returnCitizenList(){
-  if(this.state.citizensList.length == 0){
-    this.getCitizensList()
-  }
-  return(this.state.citizensList)
+
+returnSchedulesList(){
+    if(this.state.schedulesList.length == 0){
+      this.getSchedulesList()
+    }
+    console.log(this.state.schedulesList)
+    return(this.state.schedulesList)
 }
 
-getCitizensList(){
+getSchedulesList(){
   const apiUrl = `http://${apiHost}:${apiPort}/${apiVer}`;
-    const collection = `citizens`;
+    const collection = `schedules`;
     const params = `permission=${this.props.user.current_role}`
     fetch(`${apiUrl}/${collection}?${params}`, {
       headers: {
@@ -43,7 +47,7 @@ getCitizensList(){
         "Content-Type": "application/json" },
         method: "get"
     }).then(parseResponse).then(resp => {
-      this.setState({"citizensList":resp}, () => console.log(this.state))
+      this.setState({"schedulesList":resp}, () => console.log(this.state))
     }).catch(({errors}) => {
       if(errors) {
         let full_error_msg = "";
@@ -60,7 +64,9 @@ getCitizensList(){
   }
 
   printPDF(){
-        var pdf = new jsPDF('p', 'pt', 'letter');
+        var pdf = new jsPDF('p', 'pt', "a4");
+        pdf.setFont("helvetica");
+        pdf.setFontSize(1);
       var source = $('#divtoPDF')[0];
       var specialElementHandlers = {
         '#bypassme': function(element, renderer) {
@@ -68,30 +74,70 @@ getCitizensList(){
         }
       };
 
-      var margins = {
-        top: 50,
-        left: 60,
-        width: 545
-      };
+      var margins = { top: 50,left: 10,right:10, width: 650};
 
       pdf.fromHTML (
-        source // HTML string or DOM elem ref.
-        , margins.left // x coord
-        , margins.top // y coord
-        , {
-          'width': margins.width // max width of content on PDF
-          , 'elementHandlers': specialElementHandlers
-        },
+        source, margins.left, margins.top,  {'width': margins.width, 'elementHandlers': specialElementHandlers},
         function (dispose) {
-          // dispose: object with X, Y of the last line add to the PDF
-          // this allow the insertion of new lines after html
           pdf.save('relatorio_cidadaos.pdf');
         }
       )
   }
 
+  formatDateTime(dateTime){
+    var dateText = new Date(dateTime)
+    var returnText = this.addZeroBefore(dateText.getHours()) + ":" + this.addZeroBefore(dateText.getMinutes()) + " - " + dateText.getDate() + "/" + (dateText.getMonth() + 1) + "/" + dateText.getFullYear()
+    //console.log(returnText)
+    return(returnText)
+  }
+
+  addZeroBefore(time){
+    var newTime = time.toString()
+    if(newTime.length == 1){
+      newTime = "0" + time
+    }
+    return(newTime)
+  }
+
 render() {
-    return (<p>Agendamentos</p>)
+    return (
+      <div className="contentWrapper">
+        <div id="divtoPDF">
+          <Table className="bordered striped" style={{fontSize:"70%"}}>
+            <thead>
+              <tr>
+                <th>CPF do cidadão</th>
+                <th>Nome do cidadão</th>
+                <th>Número do agendamento</th>
+                <th>Profissional</th>
+                <th>Tipo do agendamento</th>
+                <th>Inicio</th>
+                <th>Fim</th>
+                <th>Situação</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.returnSchedulesList().map(function(element,i){
+                    return(
+                      <tr key={i}>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>{element.id}</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>{this.formatDateTime(element.service_start_time)}</td>
+                        <td>{this.formatDateTime(element.service_end_time)}</td>
+                        <td>{element.situation.description}</td>
+                      </tr>
+                  );
+                },this)}
+            </tbody>
+          </Table>
+          <Button onClick={this.printPDF}>Relatório em PDF</Button>
+        </div>
+
+      </div>
+    )
   }
 }
 
