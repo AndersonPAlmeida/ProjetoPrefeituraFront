bc4bb2f612d4620dfea28d2cdec839a26fe7c8af
André Starosta <afas15@inf.ufpr.br>
2017-12-14 10:39:30 -0200

    AGILE#290: Change pdf creation for user report

--- src/containers/reports/myReport.jsx
+++ src/containers/reports/myReport.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 import { browserHistory } from 'react-router'
 import { port, apiHost, apiPort, apiVer } from '../../../config/env'
 import {parseResponse} from "../../redux-auth/utils/handle-fetch-response"
-import {Row, Col, Table} from 'react-materialize'
+import {Row, Col, Table, Button} from 'react-materialize'
 import {fetch} from "../../redux-auth";
 import { connect } from 'react-redux'
 import { findDOMNode } from 'react-dom';
@@ -12,50 +12,83 @@ import ReactDOM from 'react-dom';
 import html2canvas from 'html2canvas';
 import styles from './styles/MyReport.css';
 import { LogoImage } from '../images'
-var jsPDF
+var jspdf
+var jspdfautotable
 
 
 class getMyReport extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      rows: [],
+      columns: []
+    }
     this.pdfToHTML=this.pdfToHTML.bind(this);
-
   }
   componentDidMount(){
-    console.log(this.props.user)
-    jsPDF = require('jspdf')
+    this.setState({"rows": [
+      ["CPF",this.formatCpf(this.props.user.citizen.cpf)],
+      ["RG",this.props.user.citizen.rg],
+      ["Data de Nascimento",this.formatDate(this.props.user.citizen.birth_date)],
+      ["Email",this.props.user.citizen.email],
+      ["Cidade",this.props.user.citizen.city.name],
+      ["Estado",this.props.user.citizen.state.name],
+      ["Endereço",this.props.user.citizen.address.address +', '+this.props.user.citizen.address.neighborhood],
+      ["CEP",this.props.user.citizen.address.zipcode],
+      ["Telefone",this.props.user.citizen.phone1],
+      ["Telefone 2",this.props.user.citizen.phone2]
+      ]
+    })
+    this.setState({"columns":
+      ["Dados Cadastrais"," "]
+    })
+    jspdf = require('jspdf')
+    jspdfautotable = require('jspdf-autotable')
+  }
 
+
+  formatDateTime(dateTime){
+    var dateText = new Date(dateTime)
+    var returnText = this.addZeroBefore(dateText.getHours()) + ":" + this.addZeroBefore(dateText.getMinutes()) + " - " + dateText.getDate() + "/" + (dateText.getMonth() + 1) + "/" + dateText.getFullYear()
+    //console.log(returnText)
+    return(returnText)
+  }
+
+  addZeroBefore(time){
+    var newTime = time.toString()
+    if(newTime.length == 1){
+      newTime = "0" + time
+    }
+    return(newTime)
   }
 
   pdfToHTML(){
-    var pdf = new jsPDF('p', 'pt', 'letter');
-    var source = $('#HTMLtoPDF')[0];
-    var specialElementHandlers = {
-      '#bypassme': function(element, renderer) {
-        return true
-      }
-    };
+    var doc = new jspdf('p', 'pt');
+    var name = this.props.user.citizen.name
+    var nowTime = new Date()
+    var dateURL = "Criado em " + window.location.href + "  " + this.formatDateTime(nowTime)
 
-    var margins = {
-      top: 50,
-      left: 60,
-      width: 545
-    };
+    doc.autoTable(this.state.columns, this.state.rows, {
+      columnStyles: {id: {fillColor: 255}},
+        margin: {top: 60},
+    addPageContent: function(data) {
+      //header
+      doc.setFontSize(20);
+      doc.setTextColor(40);
+      doc.setFontStyle('normal');
+      doc.text("Relatório Cadastral", data.settings.margin.left + 15, 22);
+      doc.text(name, data.settings.margin.left + 15, 50);
 
-    pdf.fromHTML (
-      source // HTML string or DOM elem ref.
-      , margins.left // x coord
-      , margins.top // y coord
-      , {
-        'width': margins.width // max width of content on PDF
-        , 'elementHandlers': specialElementHandlers
-      },
-      function (dispose) {
-        // dispose: object with X, Y of the last line add to the PDF
-        // this allow the insertion of new lines after html
-        pdf.save('relatorio_cadastral.pdf');
-      }
-    )
+      //footer
+      var str = "Página " + data.pageCount;
+      doc.setFontSize(10);
+      doc.text(str, data.settings.margin.left, doc.internal.pageSize.height - 10);
+      doc.text(dateURL, data.settings.margin.left + 100, doc.internal.pageSize.height - 10)
+    }
+});
+
+    doc.save("relatorio_cadastral.pdf");
+
   }
 
 
@@ -72,12 +105,9 @@ class getMyReport extends Component {
       <div className="card dataContainer">
         <div className="card-content">
           <div id="HTMLtoPDF" className="mt4" >
-            <div>
-              <img alt="Logo agendador" src={LogoImage} width="64px"/>
-            </div>
                 <h5 ><b>Cadastro: </b>{this.props.user.citizen.name}</h5>
 
-            <Table className="bordered striped">
+            <Table className="bordered striped" id="tabela">
               <thead>
                 <tr>
                   <th>Dados cadastrais</th>
@@ -128,7 +158,7 @@ class getMyReport extends Component {
               </tbody>
             </Table>
           </div>
-          <button onClick={this.pdfToHTML}>Baixar Relatório em PDF</button>
+          <Button onClick={this.pdfToHTML}>Baixar Relatório em PDF</Button>
         </div>
       </div>
     </div>
