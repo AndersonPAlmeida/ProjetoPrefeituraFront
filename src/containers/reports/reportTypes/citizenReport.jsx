4ee76b24c831eb4b7af268468cbc48d098c34e75
André Starosta <afas15@inf.ufpr.br>
2017-12-18 14:18:52 -0200

    AGILE#290: Add functional citizen report

--- src/containers/reports/reportTypes/citizenReport.jsx
+++ src/containers/reports/reportTypes/citizenReport.jsx
@@ -10,22 +10,24 @@ import { findDOMNode } from 'react-dom';
 import ReactDOMServer from 'react-dom/server';
 import ReactDOM from 'react-dom';
 import styles from './styles/citizenReport.css'
-var jsPDF
+import ReportPDF from '../reportPDF';
 
 
 class getCitizenReport extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      citizensList: []
+      citizensList: [],
+      sortType:0,
+      cols:[],
+      rows:[]
     }
-    this.getCitizensList = this.getCitizensList.bind(this)
+    this.getDatafromBackEnd = this.getDatafromBackEnd.bind(this)
     this.returnCitizenList = this.returnCitizenList.bind(this)
+    this.handleDecisionChange = this.handleDecisionChange.bind(this)
+    this.arrangeData = this.arrangeData.bind(this)
   }
 
-  componentDidMount(){
-    jsPDF = require('jspdf')
-  }
 returnCitizenList(){
   if(this.state.citizensList.length == 0){
     this.getCitizensList()
@@ -33,17 +35,27 @@ returnCitizenList(){
   return(this.state.citizensList)
 }
 
-getCitizensList(){
-  const apiUrl = `http://${apiHost}:${apiPort}/${apiVer}`;
+componentDidMount(){
+  this.getDatafromBackEnd("0");
+}
+
+getDatafromBackEnd(sortType){
+    var sortValue = "name"
+    if(sortType == '0'){
+      sortValue = "name"
+    }else{
+      sortValue = "birth_date"
+    }
+    const apiUrl = `http://${apiHost}:${apiPort}/${apiVer}`;
     const collection = `citizens`;
-    const params = `permission=${this.props.user.current_role}`
+    const params = `permission=${this.props.user.current_role}&q[s]=${sortValue}+asc`
     fetch(`${apiUrl}/${collection}?${params}`, {
       headers: {
         "Accept": "application/json",
         "Content-Type": "application/json" },
         method: "get"
     }).then(parseResponse).then(resp => {
-      this.setState({"citizensList":resp}, () => console.log(this.state))
+      this.setState({"citizensList":resp}, () =>this.arrangeData())
     }).catch(({errors}) => {
       if(errors) {
         let full_error_msg = "";
@@ -55,73 +67,43 @@ getCitizensList(){
     )
 }
 
+  arrangeData(){
+    console.log("arrange")
+    var i
+    this.setState({"cols":["Nome","CPF","Data de nascimento"]})
+    var protoRows = []
+    console.log(this.state)
+    for(i = 0; i < this.state.citizensList.entries.length; i++){
+      protoRows.push([ this.state.citizensList.entries[i].name, this.state.citizensList.entries[i].cpf, this.formatDate(this.state.citizensList.entries[i].birth_date)] )
+      console.log(protoRows)
+    }
+    this.setState({"rows":protoRows})
+  }
+
+
   formatDate(date){
     return(date[8] + date[9] + '/' + date[5] + date[6] + '/' + date[0] + date[1] + date[2] + date[3])
   }
-  printPDF(){
-        var pdf = new jsPDF('p', 'pt', 'letter');
-      var source = $('#divtoPDF')[0];
-      var specialElementHandlers = {
-        '#bypassme': function(element, renderer) {
-          return true
-        }
-      };
 
-      var margins = {
-        top: 50,
-        left: 60,
-        width: 545
-      };
 
-      pdf.fromHTML (
-        source // HTML string or DOM elem ref.
-        , margins.left // x coord
-        , margins.top // y coord
-        , {
-          'width': margins.width // max width of content on PDF
-          , 'elementHandlers': specialElementHandlers
-        },
-        function (dispose) {
-          // dispose: object with X, Y of the last line add to the PDF
-          // this allow the insertion of new lines after html
-          pdf.save('relatorio_cidadaos.pdf');
-        }
-      )
-  }
+
+handleDecisionChange(e){
+  this.setState({"sortType":e.target.value})
+}
 
 render() {
     return (
       <div className="contentWrapper">
-        <div id="divtoPDF">
-          <Table className="bordered striped">
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>CPF</th>
-                <th>RG</th>
-                <th>Nascimento</th>
-                <th>Telefone 1</th>
-                <th>Telefone 2</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.returnCitizenList().map(function(element,i){
-                    return(
-                      <tr key={i}>
-                        <td>{element.name}</td>
-                        <td>{element.cpf}</td>
-                        <td>{element.rg}</td>
-                        <td>{this.formatDate(element.birth_date)}</td>
-                        <td>{element.phone1}</td>
-                        <td>{element.phone2}</td>
-                    </tr>
-                  );
-                },this)}
-            </tbody>
-          </Table>
-          <Button onClick={this.printPDF}>Relatório em PDF</Button>
-        </div>
+        <h5>Opções do relatório</h5>
+        <Row>
+          <Input s={6} type='select' defaultValue='0' onChange={this.handleDecisionChange}>
+            <option value='0'>Ordenar por nome</option>
+            <option value='1'>Ordenar por data de nascimento</option>
+          </Input>
+          <Button  style={{marginTop:"1em"}} onClick={()=>this.getDatafromBackEnd(this.state.sortType)} >Salvar opção</Button>
 
+        </Row>
+        <ReportPDF h1="Relatório de cidadãos" h2="" rows={this.state.rows} cols={this.state.cols} filename="relatorio_cidadaos.pdf"/>
       </div>
     )
   }
