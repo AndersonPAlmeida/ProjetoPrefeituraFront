8bf9af24ae83ee7489e16910071a4a660ca5ae82
André Starosta <afas15@inf.ufpr.br>
2018-01-19 09:54:09 -0200

    AGILE#308: Add prototype for service report

--- src/containers/reports/reportTypes/servicesReport.jsx
+++ src/containers/reports/reportTypes/servicesReport.jsx
@@ -10,22 +10,47 @@ import { findDOMNode } from 'react-dom';
 import ReactDOMServer from 'react-dom/server';
 import ReactDOM from 'react-dom';
 import styles from './styles/servicesReport.css'
-var jsPDF
+import ReportPDF from '../reportPDF'
 
 
 class getservicesReport extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      servicesList: []
+      servicesList: [],
+      servicesIndex:[],
+      requestState:0
     }
     this.getservicesList = this.getservicesList.bind(this)
     this.returnservicesList = this.returnservicesList.bind(this)
+    this.getServiceIndex = this.getServiceIndex.bind(this)
   }
 
   componentDidMount(){
-    jsPDF = require('jspdf')
     this.getservicesList()
+  }
+
+  getServiceIndex(){
+      const apiUrl = `http://${apiHost}:${apiPort}/${apiVer}`;
+      const collection = `forms/schedule_index`;
+      const params = `permission=${this.props.user.current_role}`
+      fetch(`${apiUrl}/${collection}?${params}`, {
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json" },
+          method: "get"
+      }).then(parseResponse).then(resp => {
+
+        this.setState({"servicesIndex":resp})
+      }).catch(({errors}) => {
+        if(errors) {
+          let full_error_msg = "";
+          errors.forEach(function(elem){ full_error_msg += elem + '\n' });
+          Materialize.toast(full_error_msg, 10000, "red",function(){$("#toast-container").remove()});
+          throw errors;
+          }
+        }
+      )
   }
 
 returnservicesList(){
@@ -89,35 +114,54 @@ getservicesList(){
     return(returnText)
   }
 
+
+
 render() {
     return (
       <div className="contentWrapper">
-        <div id="divtoPDF">
-          <Table className="bordered striped" style={{fontSize:"70%"}}>
-            <thead>
-              <tr>
-                <th>Nome do profissional</th>
-                <th>CPF</th>
-                <th>Registro</th>
-                <th>Papéis</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.returnservicesList().map(function(element,i){
-                    return(
-                      <tr key={i}>
-                        <td>{element.name}</td>
-                        <td>{element.cpf}</td>
-                        <td>{element.registration}</td>
-                        <td>{this.arrangeList(element.roles_names)}</td>
-                      </tr>
-                  );
-                },this)}
-            </tbody>
-          </Table>
-          <Button onClick={this.printPDF}>Relatório em PDF</Button>
-        </div>
+        <h5>Filtros do relatório</h5>
+          <div>
+            <br/>
+            <Row>
+            <Input id="filter0" s={4} label="Local do Atendimento" type="select" default="-1" value={this.state.filterServicePlace} onChange={this.updateFilters}>
+              <option value="-1">Nenhum</option>
 
+            </Input>
+            <Input id="filter1" s={4} name='on' type='date' label="Data de Início" value={this.state.filterStartDate} onChange={this.updateFilters} />
+            <Input id="filter2" s={4} name='on' type='date' label="Data de Fim" value={this.state.filterEndDate} onChange={this.updateFilters}/>
+          </Row>
+          <Row>
+
+            <Input id="filter3" s={4} label="Profissional" type="select" default="-1" value={this.state.filterProfessional} onChange={this.updateFilters}>
+              <option value="-1">Nenhum</option>
+
+              </Input>
+
+            <Input id="filter4" s={4} label="Tipo de atendimento" type="select" default="-1" value={this.state.filterServiceType} onChange={this.updateFilters}>
+              <option value="-1" >Nenhum</option>
+
+            </Input>
+
+            <Input id="filter5" s={4} label="Situação" type="select" default="-1" value={this.state.filterSituation} onChange={this.updateFilters}>
+              <option value="-1">Nenhum</option>
+
+            </Input>
+          </Row>
+
+          <Row>
+            <Input id="filter6" s={4} label="Ordernar por" type="select" default="-1" value={this.state.filterSort} onChange={this.updateFilters}>
+                <option value="-1">Nenhum</option>
+
+            </Input>
+            <Input id="filter7" s={4} name='on' label="CPF" value={this.state.filterCPF} onChange={this.updateFilters} />
+            <Input id="filter8" s={4} name='on' label="Nome do Cidadão" value={this.state.filterName} onChange={this.updateFilters} />
+          </Row>
+          </div>
+          <Button style={{marginRight:"1rem"}} onClick={this.clearFields}>Limpar Campos</Button>
+          {this.state.requestState == "0"
+            ?(<Button onClick={this.confirmFilters}>Confirmar filtros</Button>)
+            :(<ReportPDF h1="Relatório de Agendamentos" h2="" cols={this.state.cols} rows={this.state.rows} filename="relatorio_agendamentoss.pdf" o='l'/>)
+          }
       </div>
     )
   }
