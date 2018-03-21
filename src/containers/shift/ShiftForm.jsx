ea248f8d4f27db9c58080b8e98ac01e60bb8a88a
Lucas Braz Cunha <lbc16@inf.ufpr.br>
2018-03-21 10:20:23 -0300

    AGILE#268: [WP] Calendar layout & layout organization
    Signed-off-by: Lucas Braz Cunha <lbc16@inf.ufpr.br>

--- src/containers/shift/ShiftForm.jsx
+++ src/containers/shift/ShiftForm.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { port, apiHost, apiPort, apiVer } from '../../../config/env';
 import {parseResponse} from "../../redux-auth/utils/handle-fetch-response";
 import {fetch} from "../../redux-auth";
+import DayPicker, {DateUtils } from 'react-day-picker'
+import DayPickerInput from 'react-day-picker/DayPickerInput';
 import update from 'react-addons-update';
 import { Button, Card, Row, Col, Dropdown, Input, CardPanel } from 'react-materialize'
 import styles from './styles/ShiftForm.css'
@@ -20,6 +22,11 @@ class getShiftForm extends Component {
 
  constructor(props) {
     super(props)
+
+    var today = new Date()
+    var today_3months = new Date()
+    today_3months.setMonth(today_3months.getMonth()+3)
+
     this.state = {
       city_halls: [],
       forms: [],
@@ -31,7 +38,20 @@ class getShiftForm extends Component {
         selected_service_place: 0,
         selected_professional: 0,
         selected_service_type: 0
-      }
+      },
+      aux: {
+        end_year_id: '',
+        start_year_id: '',
+        start_year: '',
+        end_year: '',
+        start_month : '',
+        end_month: '',
+        start_day: '',
+        end_day: ''
+      },
+      disabledDays: [{ after: today_3months, before: today }],
+      selected_days: [],
+      isTimeDivision: false
     };
   }
 
@@ -42,8 +62,13 @@ class getShiftForm extends Component {
       var data = this.props.is_edit ? this.props.data : this.state.shift
 
       const apiUrl = `http://${apiHost}:${apiPort}/${apiVer}`;
+
+
       var collection = 'forms/create_shift';
+      //var collection = 'forms/schedule_index';
+
       const params = this.props.fetch_params;
+      //const params = 'permission=2';
 
       fetch(`${apiUrl}/${collection}?${params}`, {
         headers: {
@@ -72,21 +97,46 @@ class getShiftForm extends Component {
 
 
       self.setState({ service_type: data })
+
     }
 
 
-    calendarComponent() {
-      return (
-        <div>
-            <Row>
-              <Col s={12}
-                className='card-panel calendar-panel'>
-                  <Input name='on' type='date' />
-              </Col>
-              </Row>
-        </div>
-              )
-    }
+    handleDayClick = (day, modifiers) => {
+  	    //encontrar o dia no array de dias, remover ele e atualizar o estado;
+        let selectedDays = this.state.selected_days;
+        let i;
+        let removed = false;
+
+         for (i = 0; i < selectedDays.length && !removed; i++) {
+            if(selectedDays[i].getTime() === day.getTime()){
+              selectedDays.splice(i,1);
+              removed = true;
+            }
+          }
+          if(i == selectedDays.length && !removed){
+            selectedDays.push(day);
+          }
+  	      this.setState({ selected_days: selectedDays});
+          console.log(this.state);
+  	  };
+
+  calendarComponent() {
+    		return (
+    								<DayPicker
+    										enableOutsideDays
+    										locale="pt"
+    										modifiers={ {
+    												listDays: this.state.selected_days
+    											} }
+    								        months={MONTHS}
+    								        weekdaysLong={WEEKDAYS_LONG}
+    								        weekdaysShort={WEEKDAYS_SHORT}
+                            onDayClick={this.handleDayClick.bind(this)}
+                            disabledDays={this.state.disabledDays}
+    					          		/>
+            )
+    	}
+
 
   pickCityHall() {
     if(this.props.current_role && this.props.current_role.role != 'adm_c3sl') {
@@ -136,6 +186,21 @@ class getShiftForm extends Component {
     )
   }
 
+  handleDateInputChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    if(target.validity.valid) {
+      this.setState({
+        aux: update(this.state.aux, { [name]: {$set: value} })
+      })
+      console.log('handleDateInputChange');
+      {
+        console.log(this.state.aux);
+      }
+    }
+  }
+
   handleInputChange(event) {
     const target = event.target;
     const name = target.name;
@@ -147,6 +212,18 @@ class getShiftForm extends Component {
       console.log('handleInputChange');
       {console.log(this.state.shift);}
     }
+  }
+
+  handleInputCheckBoxChange(event){
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    if(target.validity.valid) {
+        this.setState({
+          isTimeDivision: value
+        });
+    }
+    console.log('alo alo');
   }
 
   pickProfessional(){
@@ -209,6 +286,134 @@ class getShiftForm extends Component {
 	    )
 	}
 
+  selectStartDate(){
+      var optionsDays = [];
+      optionsDays.push(<option key={0} value="" disabled>Dia</option>);
+      for(var i = 1; i <= 31; i++){
+        optionsDays.push(
+          <option key={i} value={i}>{i}</option>
+        );
+      }
+      var optionsMonths = []
+      optionsMonths.push(<option key={0} value="" disabled>Mês</option>);
+      var months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+      for(var i = 0; i < 12; i++){
+        optionsMonths.push(
+          <option key={i+1} value={i+1}>{months[i]}</option>
+        );
+      }
+      var optionsYears = []
+      optionsYears.push(<option key={0} value="" disabled>Ano</option>);
+      var year = new Date().getFullYear()
+      for(var i = year; i > 1900; i--){
+        optionsYears.push(
+          <option key={i-1899} value={i-1899}>{i}</option>
+        );
+      }
+      return (
+        <div>
+          <Input s={12} l={3}
+            type='select'
+            name='start_day'
+            value={this.state.aux.start_day}
+            onChange={this.handleDateInputChange.bind(this)}
+          >
+            {optionsDays}
+          </Input>
+
+          <Input s={12} l={4}
+            type='select'
+            name='start_month'
+            value={this.state.aux.start_month}
+            onChange={this.handleDateInputChange.bind(this)}
+          >
+            {optionsMonths}
+          </Input>
+
+          <Input s={12} l={4}
+            type='select'
+            name='start_year_id'
+            value={this.state.aux.start_year_id}
+            onChange={ (event) => {
+                this.handleDateInputChange.bind(this)(event)
+                this.setState({aux: update(this.state.aux,
+                  {
+                    start_year: {$set: parseInt(this.state.aux.start_year_id)+parseInt(1899)},
+                  })
+                })
+              }
+            }
+          >
+            {optionsYears}
+          </Input>
+        </div>
+      )
+  }
+
+  selectEndDate(){
+      var optionsDays = [];
+      optionsDays.push(<option key={0} value="" disabled>Dia</option>);
+      for(var i = 1; i <= 31; i++){
+        optionsDays.push(
+          <option key={i} value={i}>{i}</option>
+        );
+      }
+      var optionsMonths = []
+      optionsMonths.push(<option key={0} value="" disabled>Mês</option>);
+      var months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+      for(var i = 0; i < 12; i++){
+        optionsMonths.push(
+          <option key={i+1} value={i+1}>{months[i]}</option>
+        );
+      }
+      var optionsYears = []
+      optionsYears.push(<option key={0} value="" disabled>Ano</option>);
+      var year = new Date().getFullYear()
+      for(var i = year; i > 1900; i--){
+        optionsYears.push(
+          <option key={i-1899} value={i-1899}>{i}</option>
+        );
+      }
+      return (
+        <div>
+          <Input s={12} l={3}
+            type='select'
+            name='end_day'
+            value={this.state.aux.end_day}
+            onChange={this.handleDateInputChange.bind(this)}
+          >
+            {optionsDays}
+          </Input>
+
+          <Input s={12} l={4}
+            type='select'
+            name='end_month'
+            value={this.state.aux.end_month}
+            onChange={this.handleDateInputChange.bind(this)}
+          >
+            {optionsMonths}
+          </Input>
+
+          <Input s={12} l={4}
+            type='select'
+            name='end_year_id'
+            value={this.state.aux.end_year_id}
+            onChange={ (event) => {
+                this.handleDateInputChange.bind(this)(event)
+                this.setState({aux: update(this.state.aux,
+                  {
+                    end_year: {$set: parseInt(this.state.aux.end_year_id)+parseInt(1899)},
+                  })
+                })
+              }
+            }
+          >
+            {optionsYears}
+          </Input>
+        </div>
+      )
+  }
+
   pickServiceType(){
     let serviceTypeList = this.state.forms.service_types == null ? [] : (
       this.state.forms.service_types.map((service_type, idx) => {
@@ -247,6 +452,7 @@ class getShiftForm extends Component {
     const col_small_input = 12;
     const col_large_input = 7;
     const col_medium_input = 10;
+    var firstSelected = true;
     return (
       <Row s={12}>
         <div className='card'>
@@ -278,27 +484,27 @@ class getShiftForm extends Component {
                   </Col>
                 </Row>
 
-                <Row>
+              <Row>
                   <h6>Profissional*:</h6>
                   <Col s={col_small_input} m={col_medium_input} l={col_large_input}>
                     <div>
                       {this.pickProfessional()}
                     </div>
                   </Col>
-                </Row>
+              </Row>
 
-                <Row>
+              <Row>
                   <h6>Tipo de Atendimento*:</h6>
                   <Col s={col_small_input} m={col_medium_input} l={col_large_input}>
                     <div>
                       {this.pickServiceType()}
                     </div>
                   </Col>
-                </Row>
+              </Row>
 
-                <Row>
+              <Row>
                   <div>
-                    <h6>Duração:</h6>
+                    <h6>Duração (intervalo do dia)*:</h6>
                   </div>
                   <Row>
                     <Col s={1}>
@@ -337,22 +543,21 @@ class getShiftForm extends Component {
                 </Row>
 
 
-                <Row>
+              <Row>
                   <div s={6}>
                     <h6>A partir de*:</h6>
+                    {this.selectStartDate()}
+
                   </div>
-                  <Input name='datePicker' type='date' className='datepicker'/>
-                </Row>
-                <Row>
+              </Row>
+              <Row>
                   <div>
                     <h6>Até*:</h6>
-                  <Input name='datePicker' type='date' className='datepicker'/>
+                      {this.selectEndDate()}
                   </div>
-                </Row>
-
-
-                <h6>Incluir:</h6>
-                <Row>
+              </Row>
+              <h6>Incluir:</h6>
+              <Row>
                   <Col s={12} m={4} l={2}>
                     <Input name='day1' type='checkbox' className="filled-in checkbox" value='Dom' label='Dom' />
                   </Col>
@@ -374,9 +579,9 @@ class getShiftForm extends Component {
                   <Col s={12} m={4} l={2}>
                     <Input name='day7' type='checkbox' className="filled-in" value='Sab' label='Sab' />
                   </Col>
-                </Row>
+              </Row>
 
-                <Row>
+              <Row>
 
                   <Col s={12} m={8} l={6}>
                     <h6>Pré-visualização:</h6>
@@ -384,9 +589,9 @@ class getShiftForm extends Component {
                       {this.calendarComponent()}
                     </div>
                   </Col>
-                </Row>
+              </Row>
 
-                <Row>
+              <Row>
                   <Col s={12} m={8} l={6}>
                     <div className='card'>
                       <div className='card-content'>
@@ -419,34 +624,50 @@ class getShiftForm extends Component {
                     </div>
                   </Col>
 
-                </Row>
+              </Row>
 
-                <Row>
-                  <div className="filed-input">
+              <Row>
                     <h6>Observações</h6>
+              </Row>
+              <Row>
+                  <Col s={12} m={8} l={6}>
                     <Input type='text'/>
-                    <p>Caracteres restantes:140</p>
-                  </div>
-                </Row>
+                  </Col>
+              </Row>
+              <Row>
+                  <p>Caracteres restantes:140</p>
+              </Row>
 
-                <Row>
+              <Row>
                   <div >
-                    <h6>Atendimentos:</h6>
+                    <h6>Atendimentos*:</h6>
                     <Row>
                       <div >
                         <Input name='group1' type='radio'
-                           value='green' label='Número de atendimentos:'
+                           label='Número de atendimentos:'
+                           value={false}
+                           onChange={this.handleInputCheckBoxChange.bind(this)}
                            className='with-gap' checked='true'/>
-                         <Input type='number' min={1}/>
+                         {
+                          this.state.isTimeDivision ?
+                          <Input type='number' disabled/> :
+                          <Input type='number' min={1}/>
+                         }
                       </div>
-                    </Row>
-                    <Row>
-                      <Input name='group1' type='radio'
-                         value='green' label='Tempo de atendimento (minutos):' className='with-gap'/>
-                       <Input type='number' />
                   </Row>
-                  </div>
+                  <Row>
+                      <Input name='group1' type='radio'
+                        onChange={this.handleInputCheckBoxChange.bind(this)}
+                        value={true}
+                        label='Tempo de atendimento (minutos):' className='with-gap'/>
+                       {
+                        this.state.isTimeDivision ?
+                        <Input type='number'/> :
+                          <Input type='number' disabled/>
+                       }
                 </Row>
+                  </div>
+              </Row>
                 <p className="red-text">Campos com (*) são de preenchimento obrigatório.</p>
             </Row>
             {this.confirmButton()}
